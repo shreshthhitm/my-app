d2d8d7b212938ee73b0f22f1786b3bea178d00ec
shreshthhitm <shreshth.hitm@gmail.com>
2019-06-06 01:09:08 +0530

    initial commit

---
+++ build1/precache-manifest.bb9acc361540cbf3432d57371f43fc75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36eca208b71da572f4580b63163047ae",
+    "url": "/shreshthhitm/my-app/index.html"
+  },
+  {
+    "revision": "9480e376d5bc5a1775d0",
+    "url": "/shreshthhitm/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/shreshthhitm/my-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9480e376d5bc5a1775d0",
+    "url": "/shreshthhitm/my-app/static/js/main.fb90c2f5.chunk.js"
+  },
+  {
+    "revision": "2ffca539aa1ff3619f48",
+    "url": "/shreshthhitm/my-app/static/js/runtime~main.d5ced8c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shreshthhitm/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
